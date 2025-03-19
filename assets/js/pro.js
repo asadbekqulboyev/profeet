0b03086ffc2b53cff7a755cefd41bdf051e2d138
asadbek <asadbekqulboyev@gmail.com>
2025-03-19 14:29:05 +0500

    desctop finish

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -17,11 +17,11 @@ $(document).ready(function () {
             loop:true,
             speed: 1000,
             autoplay:{
-                speed:9000,
+                speed:10000,
             },
             navigation: {
-                prevEl: '.slide_btn.next', 
-                nextEl: '.slide_btn.prev'
+                prevEl: '.hero .slide_btn.prev', 
+                nextEl: '.hero .slide_btn.next'
            },
            pagination: {
             el: '.swiper-pagination',  // Pagination element
@@ -29,16 +29,41 @@ $(document).ready(function () {
         },
         });
     }
+    if($('.reviews__slider').length){
+
+        var swiper = new Swiper(".reviews__slider", {
+            pagination: {
+                el: ".swiper-progress-bar",
+                type: "progressbar",
+              },
+              navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              },
+            slidesPerView: 2,
+            spaceBetween: 25,
+            // pagination: {
+            //     el: ".swiper-pagination",
+            //     clickable: true,
+            // },
+            // on: {
+            //     slideChange: function () {
+            //         let totalSlides = this.slides.length;
+            //         let currentSlide = this.realIndex + 1;
+            //         let progressPercent = (currentSlide / totalSlides) * 200;
+            //         $(".swiper-progress").css("width", progressPercent + "%");
+            //     }
+            // }
+        });
+    }
     // sifr
     let started = false;
-    
     function startCounting() {
-        $(".count").each(function () {
+        $(".count").each(function(){
             let $this = $(this);
             let target = parseInt($this.attr("data-target"), 10);
             let count = 0;
             let speed = Math.max(1, target / 100);
-            
             let interval = setInterval(function () {
                 count += speed;
                 if (count >= target) {
@@ -49,15 +74,15 @@ $(document).ready(function () {
             }, 40);
         });
     }
-
-    $(window).on("scroll", function () {
-        let sectionTop = $(".numbers").offset().top;
-        let triggerPoint = $(window).scrollTop() + $(window).height() - 100;
-
-        if (sectionTop < triggerPoint && !started) {
-            started = true;
-            $(".numbers").addClass("active");
-            startCounting();
-        }
-    });
+    
 })
+$(window).on("scroll", function () {
+    let sectionTop = $(".numbers").offset().top;
+    let triggerPoint = $(window).scrollTop() + $(window).height() - 100;
+
+    if (sectionTop < triggerPoint && !started) {
+        started = true;
+        $(".numbers").addClass("active");
+        startCounting();
+    }
+});
