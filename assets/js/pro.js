75e86dbe9409804081fe0e225d452583570ee1fc
asadbek <asadbekqulboyev@gmail.com>
2025-03-19 16:29:13 +0500

    desctop finish

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -30,52 +30,39 @@ $(document).ready(function () {
         });
     }
     if($('.reviews__slider').length){
-
         var swiper = new Swiper(".reviews__slider", {
             pagination: {
                 el: ".swiper-progress-bar",
                 type: "progressbar",
               },
               navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+                nextEl: ".btn_nav.next",
+                prevEl: ".btn_nav.prev",
               },
             slidesPerView: 2,
             spaceBetween: 25,
-            // pagination: {
-            //     el: ".swiper-pagination",
-            //     clickable: true,
-            // },
-            // on: {
-            //     slideChange: function () {
-            //         let totalSlides = this.slides.length;
-            //         let currentSlide = this.realIndex + 1;
-            //         let progressPercent = (currentSlide / totalSlides) * 200;
-            //         $(".swiper-progress").css("width", progressPercent + "%");
-            //     }
-            // }
         });
     }
     // sifr
-    let started = false;
-    function startCounting() {
-        $(".count").each(function(){
-            let $this = $(this);
-            let target = parseInt($this.attr("data-target"), 10);
-            let count = 0;
-            let speed = Math.max(1, target / 100);
-            let interval = setInterval(function () {
-                count += speed;
-                if (count >= target) {
-                    count = target;
-                    clearInterval(interval);
-                }
-                $this.text(Math.floor(count).toLocaleString()); // 1 000 formatda chiqarish
-            }, 40);
-        });
-    }
     
 })
+let started = false;
+function startCounting() {
+    $(".count").each(function(){
+        let $this = $(this);
+        let target = parseInt($this.attr("data-target"), 10);
+        let count = 0;
+        let speed = Math.max(1, target / 100);
+        let interval = setInterval(function () {
+            count += speed;
+            if (count >= target) {
+                count = target;
+                clearInterval(interval);
+            }
+            $this.text(Math.floor(count).toLocaleString()); // 1 000 formatda chiqarish
+        }, 50);
+    });
+}
 $(window).on("scroll", function () {
     let sectionTop = $(".numbers").offset().top;
     let triggerPoint = $(window).scrollTop() + $(window).height() - 100;
